202f97bcaea87a06f757a267654375099783d039
aliali <aliali@test.com>
2025-03-17 08:14:29 +0300

    CRUD methods

--- src/main/java/com/example/capstone01_tuwaiqjavabootcamp/Service/MerchantStockService.java
+++ src/main/java/com/example/capstone01_tuwaiqjavabootcamp/Service/MerchantStockService.java
@@ -1,4 +1,46 @@
 package com.example.capstone01_tuwaiqjavabootcamp.Service;
 
+import com.example.capstone01_tuwaiqjavabootcamp.Model.MerchantStock;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class MerchantStockService {
+
+    ArrayList<MerchantStock> merchantStocks = new ArrayList<>();
+
+
+    public ArrayList<MerchantStock> getMerchantStock() {
+        return merchantStocks;
+    }
+
+    public boolean addMerchantStock(MerchantStock merchantStock) {
+        for (MerchantStock m:merchantStocks) {
+            if (m.getId().equals(merchantStock.getId()))
+                return false;
+        }
+        merchantStocks.add(merchantStock);
+        return true;
+    }
+
+    public boolean updateMerchantStock(String id, MerchantStock merchantStock) {
+        for (MerchantStock m:merchantStocks) {
+            if (m.getId().equals(id)) {
+                merchantStocks.set(merchantStocks.indexOf(m), merchantStock);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteMerchantStock(String id) {
+        for (MerchantStock m:merchantStocks) {
+            if (m.getId().equals(id)) {
+                merchantStocks.remove(m);
+                return true;
+            }
+        }
+        return false;
+    }
 }

a1c3ddeb2b618c6920cadfa43205cbd14e3fb2b7
aliali <aliali@test.com>
2025-03-17 08:03:51 +0300

    CRUD methods

--- src/main/java/com/example/capstone01_tuwaiqjavabootcamp/Controller/ProductController.java
+++ src/main/java/com/example/capstone01_tuwaiqjavabootcamp/Controller/ProductController.java
@@ -1,7 +1,56 @@
 package com.example.capstone01_tuwaiqjavabootcamp.Controller;
 
+import com.example.capstone01_tuwaiqjavabootcamp.ApiResponse.ApiResponse;
+import com.example.capstone01_tuwaiqjavabootcamp.Model.Product;
+import com.example.capstone01_tuwaiqjavabootcamp.Service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/product")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductService productService;
     //6- Create endpoint for getting and adding and deleting updating a Product.
+
+    @GetMapping("/get")
+    public ResponseEntity getProduct() {
+        if (productService.getProduct().isEmpty())
+            return ResponseEntity.status(400).body(new ApiResponse("there are no product"));
+        return ResponseEntity.status(200).body(productService.getProduct());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity addProduct(@RequestBody @Valid Product product, Errors errors) {
+        if (errors.hasErrors()) {
+            String message = errors.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(400).body(message);
+        }
+        if (productService.addProduct(product))
+            return ResponseEntity.status(200).body(new ApiResponse("new product is added"));
+        return ResponseEntity.status(400).body(new ApiResponse("already exist"));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateProduct(@PathVariable String id, @RequestBody @Valid Product product, Errors errors) {
+        if (errors.hasErrors()) {
+            String message = errors.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(400).body(message);
+        }
+        if (productService.updateProduct(id, product))
+            return ResponseEntity.status(200).body(new ApiResponse("product is updated"));
+        return ResponseEntity.status(400).body(new ApiResponse("not found"));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteProduct(@PathVariable String id) {
+        if (productService.deleteProduct(id))
+            return ResponseEntity.status(200).body(new ApiResponse("product is deleted"));
+        return ResponseEntity.status(400).body(new ApiResponse("not found"));
+    }
 
 }
--- src/main/java/com/example/capstone01_tuwaiqjavabootcamp/Service/ProductService.java
+++ src/main/java/com/example/capstone01_tuwaiqjavabootcamp/Service/ProductService.java
@@ -1,4 +1,49 @@
 package com.example.capstone01_tuwaiqjavabootcamp.Service;
 
+import com.example.capstone01_tuwaiqjavabootcamp.Model.Product;
+import com.example.capstone01_tuwaiqjavabootcamp.Model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class ProductService {
+
+    ArrayList<Product> products = new ArrayList<>();
+
+
+    public ArrayList<Product> getProduct() {
+        return products;
+    }
+
+    public boolean addProduct(Product product) {
+        for (Product p:products) {
+            if (p.getId().equals(product.getId()))
+                return false;
+        }
+        products.add(product);
+        return true;
+    }
+
+    public boolean updateProduct(String id, Product product) {
+        for (Product p:products) {
+            if (p.getId().equals(id)) {
+                products.set(products.indexOf(p), product);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteProduct(String id) {
+        for (Product p:products) {
+            if (p.getId().equals(id)) {
+                products.remove(p);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }

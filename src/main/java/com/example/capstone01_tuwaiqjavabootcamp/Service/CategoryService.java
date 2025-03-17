6235683e0ee82609e6a6565e1aa1caf2cb2e81de
aliali <aliali@test.com>
2025-03-17 07:44:23 +0300

    CRUD methods

--- src/main/java/com/example/capstone01_tuwaiqjavabootcamp/Service/CategoryService.java
+++ src/main/java/com/example/capstone01_tuwaiqjavabootcamp/Service/CategoryService.java
@@ -1,4 +1,47 @@
 package com.example.capstone01_tuwaiqjavabootcamp.Service;
 
+import com.example.capstone01_tuwaiqjavabootcamp.Model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class CategoryService {
+
+    ArrayList<Category> categories = new ArrayList<>();
+
+    public ArrayList<Category> getCategories() {
+        return categories;
+    }
+
+    public boolean addCategory(Category category) {
+        for (Category c : categories) {
+            if (c.getId() == category.getId()||c.getName().equals(category.getName()))
+                return false;
+        }
+        categories.add(category);
+        return true;
+    }
+
+    public boolean updateCategory(int id, Category category) {
+        for (Category c : categories) {
+            if (c.getId() == id) {
+                categories.set(categories.indexOf(c), category);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteCategory(int id) {
+        for (Category c : categories) {
+            if (c.getId() == id) {
+                categories.remove(c);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
